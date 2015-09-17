50bff545a9a9c008a43ca6d7488cdace73e0b94c
Tien <tiengbach@gmail.com>
2015-09-17 16:43:22 -0700

    Assignment1 Completion
    assignment1.html & script.js files for FEWD1: Assignment1

---
+++ script.js
@@ -0,0 +1,149 @@
+//STEP 1
+// var someMonth
+// function getMonth()
+// const currentMonth
+// var summerMonth = ["June", "July", "August"];
+// function myLibraryFunction
+
+
+//STEP 2
+// 12345678.90 
+// "Just peachy." 
+// false 
+// null 
+
+
+//STEP 3
+// var basicMath = 12 + (4 * 7) - 82;
+// var applePie = (radius / 2) * Math.PI
+
+
+//STEP 4
+// var abt_firstName
+// var abt_lastName
+// var l_address
+// var l_city
+// var l_state
+// var l_zipCode
+// var abt_yourAge
+// var c_referralSource
+// var c_mayWeContactYou
+
+
+//STEP 5
+// 1)	var abt_firstName = "Tien";
+// 2)	var abt_firstName;
+// 	abt_firstName = "Tien";
+// 3)	var abt_firstName = "Tien", c_mayWeContactYou = true;
+
+
+//STEP 6
+// 1)	var myAge = 104 + "years old";
+// 	console.log (myAge);
+// 2)	var answer = "The answer is" + true;
+// 	console.log (answer);
+// 3)	var noSense = 14 + false;
+// 	console.log (noSense);
+
+
+//STEP 7
+// var age = 14;
+// console.log ("I am " + age + " years old, not " + otherAge + "." );
+// var otherAge = 98;
+
+
+//STEP 8
+// var someString = "Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\"";
+// console.log(someString);
+
+
+//STEP 9
+// var fruit; 
+// console.log (fruit);
+
+// var person = null; 
+// console.log (person);
+
+//STEP 10
+// typeof "Panda Bear"
+// typeof 876.54
+// typeof true
+// typeof [abc, 123, xyz]
+// typeof catDog
+
+
+//STEP 11
+// alert("Hello" + " Tien Bach" + ", welcome to the JavaScript class!");
+
+
+//STEP 12
+// var name = "Tien Bach";
+// alert("Hello " + name + ", welcome to the JavaScript class!");
+
+
+//STEP 13
+// var course = "JavaScript";
+// alert(course);
+
+
+//STEP 14
+// var welcome = "Hello Zak Ruvalcaba.\nWelcome to the JavaScript class!";
+// alert(welcome);
+
+
+//STEP 15
+// var name = prompt("Please enter your name");
+// console.log("Hello " + name + ", how are you?");
+
+
+//STEP 16
+// var course = prompt("What class are you taking?");
+// console.log("Welcome to " + course + "!");
+
+
+//STEP 17
+// var x = 10, y = 20;
+// console.log (x + y);
+
+
+//STEP 18
+// var x = 20;
+// console.log (x + 20);
+
+
+//STEP 19
+// var x = 20;
+// console.log ( x * 5);
+
+
+//STEP 20
+// var x = 2;
+// console.log( x / 1 );
+
+
+//STEP 21
+// var x = 20, y = 30;
+// console.log(x == 20 && y != 35);
+
+
+//STEP 22
+// var x = 20, y = 30;
+// console.log( y > x && x < y);
+
+
+//STEP 23
+// var widget = new Object();
+// console.log(typeof widget);
+
+
+//STEP 24
+// var fruits = ["apples", "bananas", "oranges"];
+// console.log(fruits instanceof Array);
+
+
+//STEP 25
+// var fruits = ["apples", "bananas", "oranges"];
+// console.log(fruits instanceof String);
+
+
+
